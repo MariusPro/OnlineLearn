556a27aea167feff0983170b45c1d5fffdb853c2
Prodan Marius <mprodan27@gmail.com>
2020-11-04 17:42:23 +0200

    Add redux
    Signed-off-by: Prodan Marius <mprodan27@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Router>
+ReactDOM.render(
+    
+<Router>
+    <CssBaseline/>
     <div>
       <Route exact path='/' component={App} />
     </div>
-</Router>,document.getElementById('root'));
+</Router>
+,document.getElementById('root'));
